e6a9a0ee38ae220c0f66937c8b68e036183d096d
vexleet <vex_malamov@abv.bg>
2021-10-21 18:14:00 +0200

    add weapon modal

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,5 +1,5 @@
 const swiperSlides = document.querySelector(".swiper-slide[data-target]");
-
+const modalContent = document.querySelectorAll(".modal-content .close");
 
 swiperSlides.addEventListener("click", function (e) {
   const dataTargetModalId = this.dataset.target
@@ -7,3 +7,10 @@ swiperSlides.addEventListener("click", function (e) {
   document.querySelector(dataTargetModalId).style.display = "block";
 })
 
+modalContent.forEach((el) => {
+  el.addEventListener("click", function() {
+    const dataTargetModalId = this.dataset.target
+
+    document.querySelector(dataTargetModalId).style.display = "none";
+  })
+})
